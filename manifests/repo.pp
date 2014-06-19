e3481e89707910d84ef44208efe70899c17d209a
Frederik Wagner <wagner@wagit.de>
2014-06-19 15:44:46 +0200

    added correct resource ordering

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,12 +1,20 @@
 # Class salt::repo
 # Set up the package repo for rpm/deb based distros
 class salt::repo {
+  # anchor classes for correcect containment
+  # (in puppet > 3.4 'contain' would be better/easier)
+  anchor{['salt::repo_begin', 'salt::repo_end']: }
+
   case $::osfamily {
     /(Debian|Ubuntu)/: {
-      include salt::repo::deb
+      Anchor['salt::repo_begin']
+      -> class{'salt::repo::deb': }
+      -> Anchor['salt::repo_end']
     }
     /(Redhat|Centos)/: {
-      include salt::repo::rpm
+      Anchor['salt::repo_begin']
+      -> class{'salt::repo::rpm': }
+      -> Anchor['salt::repo_end']
     }
     default: {
       fail("${::osfamily} is not yet supported. Add it and send a pull request!")
